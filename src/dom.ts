774795a04124ec9418508816448b7790b41d6636
bitpshr <paulbouchon90@gmail.com>
2015-05-19 15:42:48 -0400

    CSS class manipulation: addClass, containsClass, removeClass, toggleClass

--- src/dom.ts
+++ src/dom.ts
@@ -42,6 +42,48 @@ function insertAfter(node: Node, relativeElement: Node) {
 	}
 }
 
+function validateClass(token: string): void {
+	if (token === '') {
+		throw new Error('An invalid or illegal string was specified');
+	}
+	if (/\s/.test(token)) {
+		throw new Error('String contains an invalid character');
+	}
+}
+
+/**
+ * Adds one or more CSS class names to an Element without duplication
+ *
+ * @param element The Element to which to add CSS classes
+ * @param classes An array of string CSS classes to add to the Element
+ *
+ * @example
+ * dom.addClass(document.body, 'loaded');
+ *
+ * @example
+ * dom.addClass(document.body, 'loaded', 'ready');
+ */
+export function addClass(element: Element, ...classes: string[]): void {
+	let targetElement = <any> element;
+	if (targetElement.classList) {
+		let classList: any = targetElement.classList;
+		targetElement.classList.add.apply(null, classes);
+	}
+	else {
+		let newClasses: string[] = new Array();
+		for (let className in classes) {
+			validateClass(className);
+			if (!this.contains(targetElement, className)) {
+				newClasses.push(className);
+			}
+		}
+		if (newClasses.length > 0) {
+			targetElement.className += (' ' + newClasses.join(' '));
+		}
+	}
+}
+
+
 /**
  * Retrieves an element by its ID attribute
  *
@@ -53,6 +95,27 @@ function insertAfter(node: Node, relativeElement: Node) {
  */
 export function byId(id: string): HTMLElement {
 	return document.getElementById(id);
+}
+
+/**
+ * Determines whether an ELement has a CSS class name
+ *
+ * @param element The Element to which to check for a CSS class
+ * @param className The CSS class name to check for
+ *
+ * @example
+ * var loaded = dom.containsClass(document.body, 'loaded');
+ */
+export function containsClass(element: Element, className: string): boolean {
+	let targetElement = <any> element;
+	if (targetElement.classList) {
+		let classList: any = targetElement.classList;
+		return classList.contains(className);
+	}
+	else {
+		validateClass(className);
+		return targetElement.className.indexOf(className) > -1;
+	}
 }
 
 /**
@@ -130,5 +193,73 @@ export function place(node: Node, position: Position, relativeElement: Element):
 	// Position.LastIn:
 	else {
 		relativeElement.appendChild(node);
+	}
+}
+
+/**
+ * Removes all instances of one ore more CSS class names from an Element
+ *
+ * @param element The Element from which to remove CSS classes
+ * @param classes An array of string CSS classes to remove from the Element
+ *
+ * @example
+ * dom.removeClass(document.body, 'loading');
+ *
+ * @example
+ * dom.removeClas(document.body, 'loading', 'pending');
+ */
+export function removeClass(element: Element, ...classes: string[]): void {
+	let targetElement = <any> element;
+	if (targetElement.classList) {
+		let classList: any = targetElement.classList;
+		targetElement.classList.remove.apply(null, classes);
+	}
+	else {
+		let oldClasses: string[] = targetElement.className.split(/\s+/);
+		let length = oldClasses.length;
+		for (let className in classes) {
+			validateClass(className);
+			
+			let index = oldClasses.indexOf(className);
+
+			while (index !== -1) {
+				oldClasses.splice(index, 1);
+				index = oldClasses.indexOf(className);
+			}
+		}
+		if (oldClasses.length < length) {
+			targetElement.className = oldClasses.join(' ');
+		}
+	}
+}
+
+/**
+ * Toggles the presence of a CSS class name on an Element. An optional
+ * second parameter can be used to force class addition or removal.
+ *
+ * @param element The Element to add or remove classes to or from
+ * @param className The CSS class name add or remove
+ * @param force Forces either class addition if true or class removal if flase
+ *
+ * @example
+ * dom.removeClass(document.body, 'loading');
+ *
+ * @example
+ * dom.removeClas(document.body, 'loading', 'pending');
+ */
+export function toggleClass(element: Element, className: string, force?: boolean): boolean {
+	let targetElement = <any> element;
+	if (targetElement.classList) {
+		let classList: any = targetElement.classList;
+		return targetElement.classList.toggle.apply(null, className, force);
+	}
+	else {
+		let contains = this.contains(element, className);
+		this[contains ? 'removeClass' : 'addClass'](element, className);
+
+		if (force != undefined) {
+			return force;
+		}
+		return !contains;
 	}
 }
