315bf8b1aa57b06423fe839d27fbc88be2343eab
Jason Cheatham <j.cheatham@gmail.com>
2015-05-27 08:54:19 -0400

    Ensure reads before writes; typings update
    - Use a single scheduler in `schedule` that ensures reads come before
      writes.
    - Use typings from dojo-core package rather than including a copy of the
      dojo-core typings.
    
    References #12

--- src/schedule.ts
+++ src/schedule.ts
@@ -1,14 +1,73 @@
 import { Handle } from 'dojo-core/interfaces';
 import Scheduler from 'dojo-core/Scheduler';
-import { queueAnimationTask } from 'dojo-core/queue';
+import { queueAnimationTask, QueueItem } from 'dojo-core/queue';
 
-const readScheduler = new Scheduler({ queueFunction: queueAnimationTask });
-const writeScheduler = new Scheduler({ queueFunction: queueAnimationTask });
-
-export function read(callback: (...args: any[]) => void): Handle {
-	return readScheduler.schedule(callback);
+/**
+ * Specifies a read or write operation
+ */
+enum Operation {
+	READ,
+	WRITE
 }
 
+/**
+ * A QueueItem representing a particular type of operation.
+ */
+interface DomQueueItem extends QueueItem {
+	operation: Operation;
+}
+
+/**
+ * An extension of Scheduler that allows read and write operations to be scheduled and dispatched in the proper order.
+ */
+class DomScheduler extends Scheduler {
+	/**
+	 * Overrides Scheduler#_dispatch to ensure that reads happen before writes.
+	 */
+	protected _dispatch(): void {
+		const reads: DomQueueItem[] = [];
+		const writes: DomQueueItem[] = [];
+		const queue = <DomQueueItem[]> this._queue;
+
+		for (const item of queue) {
+			(item.operation === Operation.READ ? reads : writes).push(item);
+		}
+
+		this._queue = reads.concat(writes);
+		super._dispatch();
+	}
+
+	/**
+	 * Schedules a read or write operation.
+	 */
+	scheduleOperation(callback: (...args: any[]) => void, operation: Operation): Handle {
+		const item: DomQueueItem = {
+			isActive: true,
+			callback: callback,
+			operation: operation
+		};
+		return this.schedule(item);
+	}
+}
+
+const scheduler = new DomScheduler({ queueFunction: queueAnimationTask });
+
+/**
+ * Schedules a read operation.
+ *
+ * @param callback a read operation to schedule
+ * @returns a handle that can be used to cancel the operation
+ */
+export function read(callback: (...args: any[]) => void): Handle {
+	return scheduler.scheduleOperation(callback, Operation.READ);
+}
+
+/**
+ * Schedules a write operation. All scheduled write operations will run after any scheduled read operations.
+ *
+ * @param callback a write operation to schedule
+ * @returns a handle that can be used to cancel the operation
+ */
 export function write(callback: (...args: any[]) => void): Handle {
-	return writeScheduler.schedule(callback);
+	return scheduler.scheduleOperation(callback, Operation.WRITE);
 }
