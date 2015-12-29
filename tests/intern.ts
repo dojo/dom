7eb9987c18c0812d9bfc68cd7d0996ed44e28c01
Kitson Kelly <me@kitsonkelly.com>
2015-12-29 14:33:17 +0000

    Update Intern Environments

--- tests/intern.ts
+++ tests/intern.ts
@@ -18,17 +18,11 @@ export var capabilities = {
 // Browsers to run integration testing against. Note that version numbers must be strings if used with Sauce
 // OnDemand. Options that will be permutated are browserName, version, platform, and platformVersion; any other
 // capabilities options specified for an environment will be copied as-is
-export var environments = [
-	{ browser: 'IE', browser_version: '11', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'IE', browser_version: '10', os: 'WINDOWS', os_version: '8' },
-	{ browser: 'IE', browser_version: '9', os: 'WINDOWS', os_version: '7' },
-	{ browser: 'Firefox', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'Firefox', os: 'WINDOWS', os_version: 'XP' },
-	{ browser: 'Firefox', os: 'OS X' },
-	{ browser: 'Chrome', os: 'WINDOWS', os_version: '8.1' },
-	{ browser: 'Chrome', os: 'WINDOWS', os_version: 'XP' },
-	{ browser: 'Chrome', os: 'OS X' },
-	{ browser: 'Safari', browser_version: '8', os: 'OS X' }
+export const environments = [
+	{ browserName: 'internet explorer', version: [ '9', '10', '11' ], platform: 'WINDOWS' },
+	{ browserName: 'firefox', os: 'WINDOWS' },
+	{ browserName: 'chrome', os: 'WINDOWS' },
+	{ browserName: 'Safari', version: '9', os: 'OS X' }
 ];
 
 // Maximum number of simultaneous integration tests that should be executed on the remote WebDriver service
