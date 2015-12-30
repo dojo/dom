d3fe225b05515e88f5320d02946351618557367b
Kitson Kelly <me@kitsonkelly.com>
2015-12-30 09:22:57 +0000

    Switch to Dojo 2 Loader

--- tests/intern.ts
+++ tests/intern.ts
@@ -32,6 +32,13 @@ export var maxConcurrency = 2;
 // Name of the tunnel class to use for WebDriver tests
 export var tunnel = 'BrowserStackTunnel';
 
+// The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
+// loader
+export const loaders = {
+	'host-browser': 'node_modules/dojo-loader/loader.js',
+	'host-node': 'dojo-loader'
+};
+
 // Configuration options for the module loader; any AMD configuration options supported by the specified AMD loader
 // can be used here
 export var loaderOptions = {
