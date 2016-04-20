c460562bb049758a618658c09d2b7f144118307a
Michael Van Sickle <mvansickle@sitepen.com>
2016-04-20 14:06:08 -0400

    Added test to ensure that dom.create() will create nodes that need to be created in the context of another element (e.g. table cells). Addresses issue #42

--- typings/tsd.d.ts
+++ typings/tsd.d.ts
@@ -1,3 +1,4 @@
-/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-alpha.3.d.ts"/>
+/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-pre.d.ts"/>
 /// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-beta.2.d.ts" />
 /// <reference path="../tests/typings/node/node.d.ts" />
+/// <reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts"/>
