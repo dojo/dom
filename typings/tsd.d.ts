aeb6afea9a87577307553a6dda29ffed91f39bd8
Kitson Kelly <me@kitsonkelly.com>
2016-05-26 09:03:12 +0100

    Remove typings

--- typings/tsd.d.ts
+++
@@ -1,4 +0,0 @@
-/// <reference path="../node_modules/dojo-core/typings/dojo-core/dojo-core-2.0.0-pre.d.ts"/>
-/// <reference path="../node_modules/dojo-loader/typings/dojo-loader/dojo-loader-2.0.0-beta.2.d.ts" />
-/// <reference path="../tests/typings/node/node.d.ts" />
-/// <reference path="../node_modules/dojo-core/typings/symbol-shim/symbol-shim.d.ts"/>
