eb6907f7f29e2e755dc46cfcd09f15825691f5b0
Anthony Gubler <agubler@sitepen.com>
2016-09-02 17:07:08 +0100

    prepare for flat packaging

--- tests/intern.ts
+++ tests/intern.ts
@@ -44,9 +44,9 @@ export const loaders = {
 export var loaderOptions = {
 	// Packages that should be registered with the loader in each testing environment
 	packages: [
-		{ name: 'dojo-core', location: 'node_modules/dojo-core/dist/umd' },
-		{ name: 'dojo-has', location: 'node_modules/dojo-has/dist/umd' },
-		{ name: 'dojo-shim', location: 'node_modules/dojo-shim/dist/umd' },
+		{ name: 'dojo-core', location: 'node_modules/dojo-core' },
+		{ name: 'dojo-has', location: 'node_modules/dojo-has' },
+		{ name: 'dojo-shim', location: 'node_modules/dojo-shim' },
 		{ name: 'src', location: '_build/src' },
 		{ name: 'tests', location: '_build/tests' }
 	]
