ef7ecc0fc6ebb1fadc30a1823762a2db13acb7e9
Anthony Gubler <agubler@sitepen.com>
2016-09-22 12:22:22 +0100

    update ready for the flat loader package

--- tests/intern.ts
+++ tests/intern.ts
@@ -35,7 +35,7 @@ export var tunnel = 'BrowserStackTunnel';
 // The desired AMD loader to use when running unit tests (client.html/client.js). Omit to use the default Dojo
 // loader
 export const loaders = {
-	'host-browser': 'node_modules/dojo-loader/dist/umd/loader.js',
+	'host-browser': 'node_modules/dojo-loader/loader.js',
 	'host-node': 'dojo-loader'
 };
 
