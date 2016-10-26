9e36642c32e6767fffff54a34828ad7e55858f7d
Anthony Gubler <agubler@sitepen.com>
2016-10-26 10:35:04 +0100

    change iphone platform version

--- tests/intern-saucelabs.ts
+++ tests/intern-saucelabs.ts
@@ -7,7 +7,7 @@ export const environments = [
 	{ browserName: 'chrome', platform: 'Windows 10' },
 	{ browserName: 'safari', version: '9.0', platform: 'OS X 10.11' },
 	{ browserName: 'android', deviceName: 'Google Nexus 7 HD Emulator' },
-	{ browserName: 'iphone', version: '7.1' }
+	{ browserName: 'iphone', version: '9.3' }
 ];
 
 /* SauceLabs supports more max concurrency */
